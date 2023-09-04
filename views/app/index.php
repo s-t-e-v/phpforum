7b78185e75b88c7d9f597a79ca9106c2942f6fe5
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-09-04 19:36:18 +0200

    header comment index.php view

--- views/app/index.php
+++ views/app/index.php
@@ -1,3 +1,14 @@
+<?php
+/*
+ * @Author: Steven Bandaogo 
+ * @Email: steven@sbandaogo.com
+ * @Date: 2023-09-04 19:35:07 
+ * @Last Modified by:   undefined 
+ * @Last Modified time: 2023-09-04 19:35:07
+ * @Description: Home page.
+ */
+?>
+
 <?php include(VIEWS . '_partials/header.php'); ?>
 
 
