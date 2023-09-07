0545f56e804a496d14335bcb0a38b54c7ef5d13e
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-09-07 16:37:05 +0200

    brand logo: redirect -> home page

--- views/_partials/header.php
+++ views/_partials/header.php
@@ -26,7 +26,7 @@
   <div class="d-flex flex-column" style="min-height: 100vh;">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">PHP forum</a>
+        <a class="navbar-brand" href="<?= BASE; ?>">PHP forum</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
