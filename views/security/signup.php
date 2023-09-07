095f01aa7245572e8afb5928e43cb6412498b27b
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-09-07 15:33:31 +0200

    style signup 2 + general style 1

--- views/security/signup.php
+++ views/security/signup.php
@@ -11,39 +11,37 @@
 
 <?php include(VIEWS . '_partials/header.php'); ?>
 
-<div class="form_container container p-5 rounded mx-auto bg-secondary mt-5 text-primary ">
+<main class="bg-main p-5">
 
-    <h2 class="text-center">Sign Up</h2>
+    <div class="form_container container p-5 rounded mx-auto bg-content text-primary ">
 
-    <form class="mt-5">
-        <div class="mb-3">
-            <label for="email" class="form-label">Email address</label>
-            <input type="text" class="form-control" id="email" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password">
-        </div>
-        <div class="mb-3">
-            <label for="confirmPassword" class="form-label">Password confirmation</label>
-            <input type="password" class="form-control" id="confirmPassword">
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <div class="mb-3">
-            <label for="pseudo" class="form-label">Pseudo</label>
-            <input type="text" class="form-control" id="pseudo" aria-describedby="pseudoHelp">
-        </div>
-        <div class="mb-3">
-            <label for="pp" class="form-label">Profil picture</label>
-            <input type="file" class="form-control" id="pp" aria-describedby="fileHelp">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+        <h2 class="text-center">Sign Up</h2>
 
-</div>
+        <form class="mt-5">
+            <div class="my-4 py-2">
+                <label for="email" class="form-label">Email address</label>
+                <input type="text" class="form-control rounded" id="email" aria-describedby="emailHelp">
+            </div>
+            <div class="my-4 py-2">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control rounded" id="password">
+            </div>
+            <div class="my-4 py-2">
+                <label for="confirmPassword" class="form-label">Password confirmation</label>
+                <input type="password" class="form-control rounded" id="confirmPassword">
+            </div>
+            <div class="my-4 py-2">
+                <label for="pseudo" class="form-label">Pseudo</label>
+                <input type="text" class="form-control rounded" id="pseudo" aria-describedby="pseudoHelp">
+            </div>
+            <div class="my-4 py-2 w-75">
+                <label for="pp" class="form-label">Profil picture</label>
+                <input type="file" class="form-control rounded" id="pp" aria-describedby="fileHelp">
+            </div>
+            <button type="submit" class="mt-4 btn btn-primary rounded">Submit</button>
+        </form>
+
+    </div>
+</main>
 
 <?php include(VIEWS . '_partials/footer.php'); ?>
