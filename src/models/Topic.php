88be6f11b83fd94c9dce6fa144283fcf48745722
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-09-09 15:37:43 +0200

    Topic model add

--- src/models/Topic.php
+++ src/models/Topic.php
@@ -4,7 +4,7 @@
  * @Email: steven@sbandaogo.com
  * @Date: 2023-09-09 15:02:00 
  * @Last Modified by: Steven Bandaogo
- * @Last Modified time: 2023-09-09 15:03:38
+ * @Last Modified time: 2023-09-09 15:32:23
  * @Description: Topic database management
  */
 
@@ -13,7 +13,13 @@ class Topic extends Db
     /**
      * Add the topic to the database
      */
-    public static function add($topic)
+    public static function add($data)
     {
+        $pdo = self::getDb();
+        $request = "INSERT INTO topic (title, id_user, id_forum, created_at) VALUES (:title, :id_user, :id_forum, :created_at)";
+        $response = $pdo->prepare($request);
+        $response->execute(self::htmlspecialchars($data));
+
+        return $pdo->lastInsertId();
     }
 }
