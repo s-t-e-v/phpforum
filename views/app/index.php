9997ee5da3356d5e5153d73afe97b4d59150487d
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-09-10 01:13:02 +0200

    fixes error message topic form display issue

--- views/app/index.php
+++ views/app/index.php
@@ -20,8 +20,10 @@
         <?php if (isset($_SESSION['user'])) : ?>
             <h2 class="text-center text-light">Create a topic</h2>
             <form method="post" action="" class="topic_form mx-auto">
-                <div class="form-group d-flex align-items-md-center flex-column flex-md-row">
-                    <label for="title" class="text-light mb-2 mb-md-0 fs-5">Topic name</label>
+                <div class="form-group d-flex align-items-md-start flex-column flex-md-row">
+                    <div class="topic_form_label d-flex align-items-md-center">
+                        <label for="title" class="text-light mb-2 mb-md-0 fs-5">Topic name</label>
+                    </div>
                     <div class="flex-grow-1 mb-3 mb-md-0 ms-md-2">
                         <input type="text" class="form-control rounded fs-5" name="title" id="title" value="<?= $_POST["topic"] ?? ""; ?>">
                         <small class="text_error"><?= $error['title'] ?? ""; ?></small>
@@ -35,7 +37,5 @@
 
     </div>
 </main>
-
-
 
 <?php include(VIEWS . '_partials/footer.php'); ?>
