c9fe3d34ba4a9bdef236b951e4a4fc412966ebfe
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-09-09 17:40:47 +0200

    default forum session

--- views/_partials/header.php
+++ views/_partials/header.php
@@ -4,7 +4,7 @@
  * @Email: steven@sbandaogo.com
  * @Date: 2023-09-04 19:28:57 
  * @Last Modified by: Steven Bandaogo
- * @Last Modified time: 2023-09-07 00:57:06
+ * @Last Modified time: 2023-09-09 17:40:28
  * @Description: This is the header part of the webpages. Contains meta data, navbar and session message display.
  */
 ?>
