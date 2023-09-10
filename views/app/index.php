58a32e9319e265fb658c5f256616130b55637ee3
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-09-10 02:13:29 +0200

    home page layout spacing

--- views/app/index.php
+++ views/app/index.php
@@ -14,26 +14,28 @@
 <?php include(VIEWS . '_partials/header.php'); ?>
 
 <main class="flex-grow-1">
-    <div class="container">
+    <div class="container mb-5">
 
-        <!-- Topic creation form -->
-        <?php if (isset($_SESSION['user'])) : ?>
-            <h2 class="text-center text-light">Create a topic</h2>
-            <form method="post" action="" class="topic_form mx-auto">
-                <div class="form-group d-flex align-items-md-start flex-column flex-md-row">
-                    <div class="topic_form_label d-flex align-items-md-center">
-                        <label for="title" class="text-light mb-2 mb-md-0 fs-5">Topic name</label>
+        <div class="mb-5">
+            <!-- Topic creation form -->
+            <?php if (isset($_SESSION['user'])) : ?>
+                <h2 class="text-center text-light">Create a topic</h2>
+                <form method="post" action="" class="topic_form mx-auto">
+                    <div class="form-group d-flex align-items-md-start flex-column flex-md-row">
+                        <div class="topic_form_label d-flex align-items-md-center">
+                            <label for="title" class="text-light mb-2 mb-md-0 fs-5">Topic name</label>
+                        </div>
+                        <div class="flex-grow-1 mb-3 mb-md-0 ms-md-2">
+                            <input type="text" class="form-control rounded fs-5" name="title" id="title" value="<?= $_POST["topic"] ?? ""; ?>">
+                            <small class="text_error"><?= $error['title'] ?? ""; ?></small>
+                        </div>
+                        <input type="submit" class="btn btn-info px-5 rounded fs-5" value="Create">
                     </div>
-                    <div class="flex-grow-1 mb-3 mb-md-0 ms-md-2">
-                        <input type="text" class="form-control rounded fs-5" name="title" id="title" value="<?= $_POST["topic"] ?? ""; ?>">
-                        <small class="text_error"><?= $error['title'] ?? ""; ?></small>
-                    </div>
-                    <input type="submit" class="btn btn-info px-5 rounded fs-5" value="Create">
-                </div>
-            </form>
-        <?php else : ?>
-            <p class="h2 text-center text-light">Login to create a new topic</p>
-        <?php endif; ?>
+                </form>
+            <?php else : ?>
+                <p class="h2 text-center text-light">Login to create a new topic</p>
+            <?php endif; ?>
+        </div>
 
         <!-- Topics listing -->
         <?php foreach ($topics as $topic) : ?>
