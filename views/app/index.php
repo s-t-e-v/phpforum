1fcb0492f673771c08aecad55eb2c0acf28bf47f
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-09-10 02:34:43 +0200

    default pp style + display b4 db change

--- views/app/index.php
+++ views/app/index.php
@@ -41,7 +41,7 @@
         <?php foreach ($topics as $topic) : ?>
             <div class="mt-2">
                 <a href="<?= BASE . 'topic/chat?id=' . $topic['id']; ?>" class="btn btn-primary rounded text-center text-light p-2 mb-2 w-100">
-                    <img src="<?= UPLOAD . $topic['picture_profil']; ?>" alt="Photo de profil" class="rounded-circle profile_picture-75">
+                    <img src="<?= $topic['picture_profil'] ? UPLOAD . $topic['picture_profil'] : ASSETS . 'img/default_pp.png'; ?>" alt="Profil picture" class="rounded-circle profile_picture-75">
                     <h3><?= $topic['title']; ?></h3>
                     <p>by: <?= $topic['nickname']; ?></p>
                     <p><?= date('d/m/Y H:i:s', strtotime($topic['created_at'])); ?></p>
