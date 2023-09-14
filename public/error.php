b28985db15349b79e3e774723367e9f88d8ccc4e
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-09-14 14:47:01 +0200

    staff contact message v1

--- public/error.php
+++ public/error.php
@@ -4,7 +4,7 @@
  * @Email: steven@sbandaogo.com
  * @Date: 2023-09-14 13:08:57 
  * @Last Modified by: Steven Bandaogo
- * @Last Modified time: 2023-09-14 13:48:40
+ * @Last Modified time: 2023-09-14 14:40:51
  * @Description: Displays a user friendly error message.
  */
 ?>
@@ -13,7 +13,8 @@
 
 
 <main class="flex-grow-1">
-    <div class="container mb-5">
+    <div class="container py-5">
+        <p class="text-light fs-5">If the issue persists, please contact the <a href="mailto:" class="text-secondary">admin staff</a>.</p>
     </div>
 </main>
 
