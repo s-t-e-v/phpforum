f8f9d20983d6028c362a2af9d497f25047432ce1
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-09-20 21:45:01 +0200

    unset purpose comment

--- views/_partials/footer.php
+++ views/_partials/footer.php
@@ -4,13 +4,16 @@
  * @Email: steven@sbandaogo.com
  * @Date: 2023-09-04 19:27:18 
  * @Last Modified by: Steven Bandaogo
- * @Last Modified time: 2023-09-20 21:33:41
+ * @Last Modified time: 2023-09-20 21:44:34
  * @Description: This is the footer part of the webpages. The bootstrap js cdn script is placed here.
  */
 ?>
 
 </div>
-<?php unset($_SESSION['is_home_page']); ?>
+<?php
+/** unsetting to avoid the superglobal affecting other pages than the home page */
+unset($_SESSION['is_home_page']);
+?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.1/js/bootstrap.bundle.min.js" integrity="sha512-1TK4hjCY5+E9H3r5+05bEGbKGyK506WaDPfPe1s/ihwRjr6OtL43zJLzOFQ+/zciONEd+sp7LwrfOCnyukPSsg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 
