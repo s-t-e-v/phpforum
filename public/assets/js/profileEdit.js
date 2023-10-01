f769d764ebd2c3d84139c828f37e1ab9b0363849
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-10-02 01:34:59 +0200

    toggle del ico topic list

--- public/assets/js/profileEdit.js
+++ public/assets/js/profileEdit.js
@@ -1,12 +1,21 @@
 "use strict";
 
 const forumList = document.querySelector(".forum_list");
+const topicList = document.querySelector(".topic_list");
 
 const toggleDelIco = (e) => {
     const delIco = e.target.nextElementSibling.querySelector("i");
     delIco.hidden = !e.target.checked;
 }
 
+const setTopicListState = (e) => {
+    toggleDelIco(e);
+}
+
 // Event listener
-forumList.addEventListener('change', toggleDelIco);
+if (forumList)
+    forumList.addEventListener('change', toggleDelIco);
+if (topicList)
+    topicList.addEventListener('change', setTopicListState);
+
 
